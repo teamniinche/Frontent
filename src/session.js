49f95292c07221fee1af087a992c9d33d9d95583
Mamadou NDOUR <ndourm9@gmail.com>
2023-10-17 16:54:37 +0000

    tentative de correction de l'erreur 'invali signature' 1

--- src/session.js
+++ src/session.js
@@ -563,7 +563,7 @@ const handleModalClick=()=>{
 }
 const handleCloudinaryModalClick=async () => {
     //     e.preventDefault();
-    const signResponse = await fetch(hostUrl+'api/mycloudinary/signuploadform');
+    const signResponse = await fetch(hostUrl+'api/mycloudinary/signuploadform/'+fileName.nameToSave);
     const signData = await signResponse.json();
 
     const url = "https://api.cloudinary.com/v1_1/" + signData.cloudname + "/auto/upload";
@@ -584,7 +584,7 @@ const handleCloudinaryModalClick=async () => {
             formData.append("timestamp", signData.timestamp);
             formData.append("signature", signData.signature);
             formData.append("eager", "c_pad,h_200,w_200|c_crop,h_200,w_200");
-            // formData.append("public_id", fileName.nameToSave);
+            formData.append("public_id", fileName.nameToSave);
             formData.append("folder", "signed_upload_demo_form/membres");
     
             fetch(url, {
