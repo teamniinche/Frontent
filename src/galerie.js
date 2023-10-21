7d73a9f255cf59ce108e6478fcf441e658e2ac19
teamniinche <142489947+teamniinche@users.noreply.github.com>
2023-10-21 22:30:15 +0000

    Update galerie.js

--- src/galerie.js
+++ src/galerie.js
@@ -100,7 +100,7 @@ export function NouvelAlbum(props) {
                           left: 0,
                           right: 0,
                           bottom: 0,
-                          backgroundColor: 'rgba(0, 0, 0, 0.7)'
+                          backgroundColor: 'rgba(0, 0, 0, 0.7)',
                             zIndex:15000,
                         },content: {
                       position: 'absolute',
