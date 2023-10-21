efde0b548ef5ce63589c7de7969da57d69788044
teamniinche <142489947+teamniinche@users.noreply.github.com>
2023-10-21 13:05:35 +0000

    Update galerie.js

--- src/galerie.js
+++ src/galerie.js
@@ -71,7 +71,7 @@ export function NouvelAlbum(props) {
                     });
                 // };
                 const Url=hostUrl+'/api/images/post/'+fileName+'.jpg'
-                Poster(Url,{numeroEnvoi:j,ordreEnvoi:k,album:nomAlbum});
+                Poster(Url,{numeroEnvoi:j,ordreEnvoi:k,imgName:fileName+'.jpg',album:nomAlbum});
                 if (modalDisplaye.last===false){
                     const Url=hostUrl+'/api/images/album/'+fileName+'.jpg'
                     Poster(Url,{album:nomAlbum}); 
