4152916633a7f67ee16bba6a2f4cddf05419e57d
teamniinche <142489947+teamniinche@users.noreply.github.com>
2023-10-22 23:11:53 +0000

    Update galerie.js

--- src/galerie.js
+++ src/galerie.js
@@ -248,9 +248,9 @@ export function PhotosGrid() {
     <div style={{paddingTop:"20px",display:"flex",flexFlow:"row wrap",alignItems:"start",justifyContent:"space-around"}}>
       {
         images.map(image=>
-        <div style={{position:"relative",width:"44vw",height:"44vw",padding:"2vw",margin:"0.40vw",borderRadius:"4px",border:"1px solid grey"}}>
-            <img src={cloudinaryBaseUrl+'/'+ image.imgName} alt='Delagalerie' style={{position:"absolute",zIndex:"0",width:"88%",height:"88%",margin:"0px",padding:"0px"}}/>
-            <div style={{position:"absolute",float:"left",zIndex:"1",display:"flex",flexFlow:"row wrap",width:"100%",height:"fit-content"}}>
+        <div style={{position:"relative",width:"44vw",height:"50vw",padding:"2vw",margin:"0.40vw",borderRadius:"4px",border:"1px solid grey"}}>
+            <img src={cloudinaryBaseUrl+'/'+ image.imgName} alt='Delagalerie' style={{position:"absolute",zIndex:"0",bottom:"0px",width:"44vw",height:"44vw",margin:"0px",padding:"0px"}}/>
+            <div style={{position:"absolute",float:"left",zIndex:"1",display:"flex",flexFlow:"row wrap",width:"100%",height:"4vw"}}>
                 <button style={{backgroundColor:"rgba(0,0,0,0)",border:"1px solid black",color:"grey",width:"fit-content",margin:"0px 1em"}} onClick={handleAdd}>{ajouter}</button>
                 <button style={{backgroundColor:"rgba(0,0,0,0)",border:"1px solid black",color:"red",width:"fit-content",margin:"0px 1em"}} onClick={handleDelete}>{supprimer}</button>
             </div>
