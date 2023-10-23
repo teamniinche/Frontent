7fccb09c053ecfc0caf0890fbffcf2feffd4d626
teamniinche <142489947+teamniinche@users.noreply.github.com>
2023-10-23 11:19:48 +0000

    Update galerie.js

--- src/galerie.js
+++ src/galerie.js
@@ -249,12 +249,12 @@ export function PhotosGrid() {
     <div style={{paddingTop:"20px",display:"flex",flexFlow:"row wrap",alignItems:"start",justifyContent:"space-around"}}>
       {
         images.map(image=>
-        <div style={{position:"relative",width:"44vw",height:"50vw",padding:"0.5vw",margin:"0.40vw",borderRadius:"4px",border:"1px solid rgba(0,0,0,0.4)"}}>
+        <div style={{position:"relative",width:"44vw",height:"50vw",padding:"0.5vw",paddingTop:"0px",margin:"0.40vw",borderRadius:"4px",border:"1px solid rgba(0,0,0,0.4)"}}>
             <img src={cloudinaryBaseUrl+'/'+ image.imgName} alt='Delagalerie' style={{position:"absolute",zIndex:"0",bottom:"0.5vw",width:"44vw",height:"45.5vw",margin:"0px",padding:"0px"}}/>
-            <div style={{position:"absolute",float:"right",zIndex:"1",display:"flex",flexFlow:"row wrap",justifyContent:"space-between",alignItems:"center",width:"44vw",height:"5.5vw"}}>
+            <div style={{position:"absolute",float:"right",zIndex:"1",display:"flex",flexFlow:"row wrap",justifyContent:"space-between",alignItems:"center",width:"44vw",paddingBottom:"0.25vw",height:"5.75vw"}}>
                 <button style={{display:"none",backgroundColor:"rgba(0,0,0,0)",border:"1px solid black",color:"red",width:"fit-content",margin:"0px 1em"}} onClick={handleAdd}>{ajouter}</button>
-                <button style={{backgroundColor:"rgba(0,0,0,0)",border:"1px solid black",color:"blue",width:"40px",margin:"0px 1em"}} onClick={handleAdd}>{ajouter}</button>
-                <button style={{backgroundColor:"rgba(0,0,0,0)",border:"1px solid black",color:"red",width:"40px",margin:"0px 1em"}} onClick={handleDelete}>{supprimer}</button>
+                <button style={{backgroundColor:"rgba(0,0,0,0)",color:"blue",width:"40px",margin:"0px 1em"}} onClick={handleAdd}>{ajouter}</button>
+                <button style={{backgroundColor:"rgba(0,0,0,0)",color:"red",width:"40px",margin:"0px 1em"}} onClick={handleDelete}>{supprimer}</button>
             </div>
         </div>)
          }
