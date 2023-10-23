8d4560283e0c637d87217bd8ddad0fc1406a329e
teamniinche <142489947+teamniinche@users.noreply.github.com>
2023-10-23 11:38:22 +0000

    Update galerie.js

--- src/galerie.js
+++ src/galerie.js
@@ -241,8 +241,8 @@ export function PhotosGrid() {
           .then(pictures => setImages(pictures))
           .catch(error => setError(error.message))}
      ,[])
-    const handleAdd=()=>{}
-    const handleDelete=()=>{}
+    const handleAdd=()=>{alert('Ajouter à votre galerie')}
+    const handleDelete=()=>{alert('Supprimer de la galerie')}
     
     const cloudinaryBaseUrl = 'https://res.cloudinary.com/dapkl1ien/image/upload/signed_upload_demo_form/galerie'
   return (
