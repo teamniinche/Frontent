ef6c21b1727b5a911555980deb0e47f6f6570dc8
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-24 20:45:56 +0000

    Update mur.js

--- src/mur.js
+++ src/mur.js
@@ -11,7 +11,7 @@ export default function Mur() {
       <span id="barDeSix"> | </span>
       <Link className="insConnect" to="/connexion/inscription">S'inscrire</Link>
   </div>
-  <h4 style={{lineHeight:"1rem",display:"inlline-block",width:"100%",color:"rgba(255,255,255,0.5",fontFamily:"sans-serif",fontSize:"1rem",position:"absolute",top:"50%",textAlign:"center",}}>
+  <h4 style={{lineHeight:"1rem",display:"inlline-block",width:"100%",color:"rgba(255,255,255,0.5",fontFamily:"sans-serif",fontSize:"1rem",position:"absolute",top:"60%",textAlign:"center",}}>
     <span style={{fontFamily:"'Sofia', sans-serif",fontSize:"3rem",}}>TeamNiintche</span>
     <br/>
     Volontaires de la citoyenneté active
