c2fc7764de02952b8191871d3810d00a9b7aa963
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-25 15:39:55 +0000

    Update nos_partenaires.js

--- src/nos_partenaires.js
+++ src/nos_partenaires.js
@@ -1,4 +1,13 @@
-import {partenaires} from './partenaires.js';
+//import {partenaires} from './partenaires.js';
+const parteners={
+    sonatel:{nom:"Sonatel",logo:"logo-sonatel.jpg",date:"01/09/2022",intervention:["Campagne TOUS A L'ÉCOLE 2021 à Kaguitte(Ziguinchor)","Campagne TOUS A L'ÉCOLE 2023 à DABO (Ziguinchor)"] ,total:"",contacts:""},
+    seter:{nom:"Seter",logo:"logo-seter.jpg",date:"01/09/2022",intervention:["Démarches en cours ..."],total:"",contacts:""},
+    auchan:{nom:"Auchan",logo:"logo-auchan.jpg",date:"01/09/2022",intervention:["Refection école Keur Massar","Réfection école Keur Madiabel(Kaolack)"],total:"",contacts:""},
+    distingo:{nom:"Distingo",logo:"logo-distingo.jpg",date:"01/09/2022",intervention:["Campagne TOUS A L'ÉCOLE 2021 à Kaguitte(Ziguinchor)","Campagne TOUS A L'ÉCOLE 2023 à DABO (Ziguinchor)"],total:"",contacts:""},
+    fabrimetal:{nom:"Fabri Metal",logo:"logo-fabrimetal.jpg",date:"01/09/2022",intervention:["Réfection école SEBI GARE (Sébikotane)"],total:"",contacts:""},
+    mazars:{nom:"Mazars",logo:"logo-mazars.jpg",date:"01/09/2022",intervention:["Réfection école Ngolar sérère(Notto Diobass)"],total:"",contacts:""},
+    senum:{nom:"Senum",logo:"logo-senum.jpg",date:"01/09/2022",intervention:["Démarches en cours ..."],total:"",contacts:""}
+}
 export default function Partenaires() {
 
 
@@ -12,7 +21,7 @@ export default function Partenaires() {
                 <th>Contacts</th>
             </thead>
             <tbody>
-               {Array.from(partenaires).map(ptner=>{ <tr>
+               {Array.from(parteners).map(ptner=>{ <tr>
                     <td>{ptner.nom}</td>
                     <td>{ptner.date}</td>
                     <td>{/* ptner.map(actvt=><li>{actvt && actvt.nom}</li>)*/}</td>
