1ef365d314be34db5f7b939b9d15317e88d47cbb
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-25 17:47:04 +0000

    Update nos_partenaires.js

--- src/nos_partenaires.js
+++ src/nos_partenaires.js
@@ -88,7 +88,7 @@ export default function Partenaires() {
                 <th>Contacts</th>
             </thead>
             <tbody>
-               {Object.values(parteners).map(ptner=>{var logo="./images/logos_partenaires/"+ptner.logo; return <tr>
+               {Object.values(parteners).map(ptner=>{var logo="./images/logos partenaires/"+ptner.logo; return <tr>
                     <td>
                        <img src={logo} width="20px" height="30px"/>
                        <br/>
