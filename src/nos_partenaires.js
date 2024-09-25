6252c13d846e214e1476a1e70a3c0339a93deefc
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-25 15:11:45 +0000

    Add files via upload

---
+++ src/nos_partenaires.js
@@ -0,0 +1,26 @@
+import {partenaires} from './src/partenaires.js';
+export default function Partenaires() {
+
+
+    return <div className='parteners' style={{width:"100%",height:"100vh",margin:"0px",padding:"2rem 10%",}}>
+        <table style={{width:"80%",height:"fit-content",}}>
+            <thead>
+                <th>Nom & Logo</th>
+                <th>Dépuis ...</th>
+                <th>Activités</th>
+                <th>Apport total</th>
+                <th>Contacts</th>
+            </thead>
+            <tbody>
+               {partenaires.map(ptner=>{ <tr>
+                    <td>{ptner.nom}</td>
+                    <td>{ptner.date}</td>
+                    <td>{ ptner.map(actvt=><li>{actvt && actvt.nom}</li>)}</td>
+                    <td>{ptner.total}</td>
+                    <td>{ptner.contacts}</td>
+                </tr>})}
+            </tbody>
+        </table>
+        </div>
+      
+  }
