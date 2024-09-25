e93ce36b2294994be18ed831aa815282fa030b46
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-25 15:07:12 +0000

    Add files via upload

---
+++ src/partenaires.js
@@ -0,0 +1,9 @@
+export const partenaires={
+    sonatel:{nom:"Sonatel",logo:"logo-sonatel.jpg",date:"",intervention:{},total:"",contacts:""},
+    seter:{nom:"Seter",logo:"logo-seter.jpg",date:"",intervention:{},total:"",contacts:""},
+    auchan:{nom:"Auchan",logo:"logo-auchan.jpg",date:"",intervention:{},total:"",contacts:""},
+    distingo:{nom:"Distingo",logo:"logo-distingo.jpg",date:"",intervention:{},total:"",contacts:""},
+    fabrimetal:{nom:"Fabri Metal",logo:"logo-fabrimetal.jpg",date:"",intervention:{},total:"",contacts:""},
+    mazars:{nom:"Mazars",logo:"logo-mazars.jpg",date:"",intervention:{},total:"",contacts:""},
+    senum:{nom:"Senum",logo:"logo-senum.jpg",date:"",intervention:{},total:"",contacts:""}
+}
