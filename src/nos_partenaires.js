ff98d0ed0134fec2cd8b92a27d15563aed06abbb
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-26 10:03:09 +0000

    Update nos_partenaires.js

--- src/nos_partenaires.js
+++ src/nos_partenaires.js
@@ -1,21 +1,6 @@
 //import {partenaires} from './partenaires.js';
 
 export default function Partenaires() {
-
-    function numStr(a, b) {
-          a = '' + a;
-          b = b || ' ';
-          var c = '',
-              d = 0;
-          while (a.match(/^0[0-9]/)) {
-            a = a.substr(1);
-          }
-          for (var i = a.length-1; i >= 0; i--) {
-            c = (d != 0 && d % 3 == 0) ? a[i] + b + c : a[i] + c;
-            d++;
-          }
-          return c;
-    }
     
     const parteners = {
     sonatel: {
@@ -85,7 +70,7 @@ export default function Partenaires() {
     }
     }
     const fontSize="0.6rem";
-    return <div className='parteners' style={{width:"80%",height:"80vh",margin:"0px",padding:"10vh 10%",paddingTop:"15vh",}}>
+    return <div className='parteners' style={{height:"80vh",margin:"0px",padding:"10vh 10%",paddingTop:"15vh",}}>
         {/*<img src="/images/teamniintche.png" width="60px" height="50px"/>*/}
         <h3 style={{color:"rgba(0,0,100,0.3)",padding:"1rem 0px",}}>La TeamNiintche remercie tous ses partenairees et collaborateurs.</h3>
         <table style={{width:"100%",height:"fit-content",}}>
