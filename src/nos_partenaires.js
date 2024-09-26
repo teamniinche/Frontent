50fb66c8a8eb9cdc7a8a67fef7620bd40427d7c9
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-26 08:39:13 +0000

    Update nos_partenaires.js

--- src/nos_partenaires.js
+++ src/nos_partenaires.js
@@ -17,10 +17,10 @@ export default function Partenaires() {
         logo: "logo_sonatel.png",
         date: "01/09/2022",
         intervention: [
-            "Campagne TOUS A L'ÉCOLE 2021 à Kaguitte(Ziguinchor)",
-            "Campagne TOUS A L'ÉCOLE 2023 à DABO (Ziguinchor)"
+            "Campagne TOUS A L'ÉCOLE 2021",
+            "Campagne TOUS A L'ÉCOLE 2023"
         ],
-        total: "10 000 000 F cfa",
+        total:10000000,
         contacts: "GROUPE SONATEL"
     },
     auchan: {
@@ -28,10 +28,10 @@ export default function Partenaires() {
         logo: "logo_auchan.png",
         date: "01/09/2022",
         intervention: [
-            "Réfection école Keur Massar",
-            "Réfection école Keur Madiabel(Kaolack)"
+            "Réfection école élémentaire LA LINGUERE de Keur Massar",
+            "Réfection école primaire de Keur Madiabel(Kaolack)"
         ],
-        total: "15 000 000 F cfa",
+        total:15000000,
         contacts: "AUCHAN SENEGAL"
     },
     distingo: {
@@ -39,32 +39,32 @@ export default function Partenaires() {
         logo: "logo_distingo.png",
         date: "01/09/2022",
         intervention: [
-            "Campagne TOUS A L'ÉCOLE 2021 à Kaguitte(Ziguinchor)",
-            "Campagne TOUS A L'ÉCOLE 2023 à DABO (Ziguinchor)"
+            "Campagne TOUS A L'ÉCOLE 2022",
+            "Campagne TOUS A L'ÉCOLE 2023"
         ],
-        total: "10 000 000 F cfa",
+        total:10000000,
         contacts: "distingo.com"
     },
     fabrimetal: {
         nom: "Fabri Metal",
         logo: "logo_fabrimetal.png",
         date: "01/09/2022",
-        intervention: ["Réfection école SEBI GARE (Sébikotane)"],
-        total: "10 000 000 F cfa",
+        intervention: ["Réfection école primaire SEBI GARE (Sébikotane)"],
+        total:10000000,
         contacts: "fabrimetal.com"
     },
     mazars: {
         nom: "Mazars",
         logo: "logo_mazars.png",
         date: "01/09/2022",
-        intervention: ["Réfection école Ngolar sérère(Notto Diobass)"],
-        total: "10 000 000 F cfa",
+        intervention: ["Réfection école primaire Ngolar sérère(Notto Diobass)"],
+        total:10000000,
         contacts: "mazars.org"
     },
     senum: {
         nom: "Senum",
         logo: "logo_senum.png",
-        date: "01/09/2022",
+        date: "01/09/2024",
         intervention: ["Démarches en cours ..."],
         total: "",
         contacts: "senum.com"
@@ -72,33 +72,22 @@ export default function Partenaires() {
     seter: {
         nom: "Seter",
         logo: "logo_seter.png",
-        date: "01/09/2022",
+        date: "01/09/2024",
         intervention: ["Démarches en cours ..."],
         total: "",
         contacts: "seter.sn"
     }
-};
-   /* const auchan=require('./images/logos_partenaires/logo_auchan.png');
-    const sonatel=require('./images/logos_partenaires/logo_sonatel.png');
-    const seter=require('./images/logos_partenaires/logo_seter.png');
-    const senum=require('./images/logos_partenaires/logo_senum.png');
-    const mazars=require('./images/logos_partenaires/logo_mazars.png');
-    const distingo=require('./images/logos_partenaires/logo_distingo.png');
-    const fabrimetal=require('./images/logos_partenaires/logo_fabrimetal.png');
-   
-
-    const trPartenaires=()=>{return 
-                    }*/
+    const fontSize="0.6rem";
     return <div className='parteners' style={{width:"80%",height:"80vh",margin:"0px",padding:"10vh 10%",paddingTop:"15vh",}}>
         {/*<img src="/images/teamniintche.png" width="60px" height="50px"/>*/}
         <h3 style={{color:"rgba(0,0,100,0.3)",padding:"1rem 0px",}}>La TeamNiintche remercie tous ses partenairees et collaborateurs.</h3>
         <table style={{width:"100%",height:"fit-content",}}>
             <thead>
                 <th style={{height:"4rem",lineHeight:"1rem",backgroundColor:"rgba(0,0,100,0.3)",}}>Nom & Logo</th>
-                <th style={{width:"8rem",backgroundColor:"rgba(0,0,100,0.3)",fontSize:"0.8rem",}}>Dépuis ...</th>
-                <th style={{backgroundColor:"rgba(0,0,100,0.3)",paddingRight:"1rem",fontSize:"0.8rem",}}>Activités</th>
-                <th style={{backgroundColor:"rgba(0,0,100,0.3)",fontSize:"0.8rem",}}>Apport total</th>
-                <th style={{backgroundColor:"rgba(0,0,100,0.3)",fontSize:"0.8rem",}}>Contacts</th>
+                <th style={{width:"8rem",backgroundColor:"rgba(0,0,100,0.3)",fontSize:fontSize,}}>Dépuis ...</th>
+                <th style={{backgroundColor:"rgba(0,0,100,0.3)",paddingRight:"1rem",fontSize:fontSize,}}>Activités</th>
+                <th style={{backgroundColor:"rgba(0,0,100,0.3)",fontSize:fontSize,minWidth:"10rem",fontWeight:"bold",textAlign:"center",}}>Apport total (F cfa)</th>
+                <th style={{backgroundColor:"rgba(0,0,100,0.3)",fontSize:fontSize,}}>Contacts</th>
             </thead>
             <tbody>{Object.values(parteners).map(ptner=>{
                    return <tr style={{height:"5rem",borderBottom:"1px solid rgba(0,0,100,0.3)",}}>
@@ -107,10 +96,10 @@ export default function Partenaires() {
                            <br/>
                            <span>{ptner.nom}</span>
                         </td>
-                        <td style={{width:"8rem",paddingRight:"1rem",fontSize:"0.8rem",}}>{ptner.date}</td>
-                        <td style={{fontSize:"0.8rem",}}>{ptner.intervention.map(actvt=>(<li>{actvt}</li>))}</td>
-                        <td style={{fontSize:"0.8rem",}}>{ptner.total}</td>
-                        <td style={{fontSize:"0.8rem",}}><a href="">{ptner.contacts}</a></td>
+                        <td style={{width:"8rem",paddingRight:"1rem",fontSize:fontSize,}}>{ptner.date}</td>
+                        <td style={{fontSize:fontSize,}}>{ptner.intervention.map(actvt=>(<li>{actvt}</li>))}</td>
+                        <td style={{fontSize:fontSize,minWidth:"10rem",fontWeight:"bold",textAlign:"center",}}>{numStr(ptner.total,' '}</td>
+                        <td style={{fontSize:fontSize,}}><a href="">{ptner.contacts}</a></td>
                     </tr>})}
             </tbody>
         </table>
