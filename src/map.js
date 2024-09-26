845cb078ac86d64be82de73f2578be88821d08b6
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-26 16:00:02 +0000

    Update map.js

--- src/map.js
+++ src/map.js
@@ -49,7 +49,7 @@ export default function Map(props) {
   return <div id="map">
     <MapContainer ref={mapRef} center={center} zoom={6.2} scrollWheelZoom={false} className='mapContainer'>
         <TileLayer
-          attribution='🇸🇳 Crédit @teamniintche'
+          attribution='SénégaL 🇸🇳 activités @teamniintche & partenaires & collaborateurs'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {sites.map((site,index)=>{
