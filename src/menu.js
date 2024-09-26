4abd8cffd7fa65c775ab4fb5c0d25b37802f066f
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-26 11:36:34 +0000

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -10,19 +10,25 @@ export default function Menu() {
           document.getElementsByClassName('secondebar')[0].style.display="flex";
         }
       }
+    const active=(page)=>{
+        var urlcourante = document.location.href;
+        const target={};
+        const activeStyle= urlcourante.includes(page)?target:{};
+        return activeStyle;
+    }
   return (
     <div id='menu' onClick={handleClick}>
         <Link style={{textDecoration:'none'}} id='fermer'><span style={{color:'red',fontSize:'20px',textAlign:'left'}}><i className="fa fa-xmark"></i></span></Link>
-        <Link to='/' className='menu_link' style={{marginTop:'10px'}}>ACCUEIL</Link>
+        <Link to='/' className='menu_link' style={{marginTop:'10px',...active("accueil")}}>ACCUEIL</Link>
         <hr/>
-        <Link to='/nos_realisations' className='menu_link'>NOS REALISATIONS</Link>
+        <Link to='/nos_realisations' className='menu_link' style={active("realisations")}>NOS REALISATIONS</Link>
         <hr/>
-        <Link to='/nos_partenaires' className='menu_link'>NOS PARTENAIRES</Link>
+        <Link to='/nos_partenaires' className='menu_link' style={active("partenaires")}>NOS PARTENAIRES</Link>
         {/*<Link to='/quisommesnous' className='menu_link'>QUI SOMMES-NOUS ?</Link>
         <hr/>
         <Link to='/' className='menu_link'>NOTRE COMPTABILITE</Link>*/}
         <hr/>
-        <Link to='/nousContacter' className='menu_link'>NOUS CONTACTER</Link>
+        <Link to='/nousContacter' className='menu_link' style={active("contact")}>NOUS CONTACTER</Link>
         <hr/>
     </div>
   )
