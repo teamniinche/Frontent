1361c6d1e3ede496f0f32deb4cc9230ab24e6037
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-26 13:08:10 +0000

    Update mur.js

--- src/mur.js
+++ src/mur.js
@@ -6,7 +6,7 @@ const imageMurale=require('./images/patriarkat.jpg')
 export default function Mur() {
   return <div className="header">
   <img className="imageDeFont" src={imageMurale} alt="murale"/>
-  <div className="divInsConnect">
+  <div className="divInsConnect" style={{alignItems:"center",}}>
       <Link className="insConnect" to="/connexion">Se connecter</Link>
       <span id="barDeSix"> | </span>
       <Link className="insConnect" to="/connexion/inscription">S'inscrire</Link>
