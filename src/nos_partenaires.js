1c17037f3cbaab41c857742e834f8f36908f8fad
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-27 10:52:38 +0000

    Update nos_partenaires.js

--- src/nos_partenaires.js
+++ src/nos_partenaires.js
@@ -81,10 +81,10 @@ export default function Partenaires() {
     
     const fontSize="0.6rem";
     return <>
-        <div className='parteners' style={{height:"400px",margin:"0px",paddingTop:"70px",maxHeight:"400px",overflowX:"scroll",}}>
+        <div className='parteners' style={{height:"87vh",margin:"0px",paddingTop:"70px",}}>
         {/*<img src="/images/teamniintche.png" width="60px" height="50px"/>*/}
         <h3 style={{color:"rgba(0,0,100,0.3)",padding:"1rem 0px",marginTop:"0px",textAlign:"center",}}>La TeamNiintche remercie tous ses partenairees et collaborateurs.</h3>
-        <table style={{width:"100%",height:"fit-content",}}>
+        <table style={{width:"100%",height:"fit-content",maxHeight:"90%",overflowX:"scroll",}}>
             <thead>
                 <th style={{height:"4rem",lineHeight:"1rem",backgroundColor:"rgba(0,0,100,0.3)",minWidth:"5rem",}}>Nom & Logo</th>
                 <th style={{width:"8rem",backgroundColor:"rgba(0,0,100,0.3)",fontSize:fontSize,textAlign:"center",}}>Dépuis ...</th>
