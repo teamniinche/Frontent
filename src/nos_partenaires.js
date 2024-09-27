b10a62ee5005196c515c04902b054f176e3570fe
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-09-27 09:48:18 +0000

    Update nos_partenaires.js

--- src/nos_partenaires.js
+++ src/nos_partenaires.js
@@ -79,8 +79,8 @@ export default function Partenaires() {
     }
 }
     const fontSize="0.6rem";
-    return <div style={{height:"87vh",}}>
-        <div className='parteners' style={{height:"100%",margin:"0px",paddingTop:"70px",maxHeight:"500px",overflow:"scroll",}}>
+    return <div style={{height:"400px",}}>
+        <div className='parteners' style={{height:"100%",margin:"0px",paddingTop:"70px",maxHeight:"300px",overflow:"scroll",}}>
         {/*<img src="/images/teamniintche.png" width="60px" height="50px"/>*/}
         <h3 style={{color:"rgba(0,0,100,0.3)",padding:"1rem 0px",marginTop:"0px",textAlign:"center",}}>La TeamNiintche remercie tous ses partenairees et collaborateurs.</h3>
         <table style={{width:"100%",height:"fit-content",}}>
