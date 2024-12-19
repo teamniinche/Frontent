c383bdc162f58375e2f86b2eeba0b18f91269d6a
Teamniintche <ndourm9@gmail.com>
2024-12-19 14:54:23 +0000

    correction de bugs,ajustements styles

--- src/sections.js
+++ src/sections.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {NousContacter} from './nousContacter.js';
+// import {NousContacter} from './nousContacter.js';
+import Footer from './footer.js';
 import './css/sections.css';
 import  SideBar from './sideBar.js';
 /* <img src="/filigrane.jpg" alt="filigrane" style={{margin:"-40px 0px",zIndex:"-10",padding:"0px",height:"70vh",width:"100vw"}} id="filigrane"/> */
@@ -8,7 +9,7 @@ export default function Sections() {
 
     return (
       <div className='sections'>
-          <NousContacter/>
+          <Footer/>
           <SideBar/>
       </div>
     )
